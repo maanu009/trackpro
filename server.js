607efb70604947b39b280f42efaf7d8d88a72dc6
maanu009 <amanmanu996@gmail.com>
2026-03-10 03:15:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,10 +20,7 @@ app.use(express.static(path.join(__dirname))); // Serve index.html
 
 // Connect to MongoDB
 mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/trackpro', {
-    serverSelectionTimeoutMS: 30000,
-    family: 4
-})
+mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/trackpro')
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.error('MongoDB Connection Error:', err));
 
